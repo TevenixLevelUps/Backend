c39dd578cff2a7bd3cda1a8981c779eee80d41b8
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-16 16:44:54 +0300

    config file created

---
+++ barber_shop/app/config.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 8000
+
+
+class DatabaseConfig(BaseModel):
+    user: str
+    password: str
+    host: str
+    port: int
+    name: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+
+    run: RunConfig = RunConfig()
+    db: DatabaseConfig
