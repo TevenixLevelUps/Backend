b18e29a300129c478a147f4717e9ea6a27638450
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-16 19:48:12 +0300

    post service

---
+++ barber_shop/app/services/router.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+from uuid import uuid4
+
+from fastapi import APIRouter, status, UploadFile, File, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import session_getter
+from app.services.dao import ServicesDAO
+from app.services.schemas import SService
+
+router = APIRouter(
+    prefix="/services",
+    tags=["Services"],
+)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_service(
+        service: SService,
+        session: AsyncSession = Depends(session_getter),
+) -> dict[str, str]:
+    await ServicesDAO.add(
+        session,
+        id=uuid4(),
+        **service.dict(),
+    )
+    await session.commit()
+    return {"message": "service added successfully"}
+
+
