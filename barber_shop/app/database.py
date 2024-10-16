f6fda2b6527c5de42dc1b03f9f1d4fd155d10f04
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-16 16:58:17 +0300

    database file with db settings

---
+++ barber_shop/app/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from app.config import settings
+
+DB_USER = settings.db.user
+DB_PASS = settings.db.password
+DB_HOST = settings.db.host
+DB_PORT = settings.db.port
+DB_NAME = settings.db.name
+
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
