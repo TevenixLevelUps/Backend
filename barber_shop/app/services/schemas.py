96213442c5e6186bd55ca56edc51f8f5acf9aa58
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-16 19:43:41 +0300

    service schema update

--- barber_shop/app/services/schemas.py
+++ barber_shop/app/services/schemas.py
@@ -1,11 +1,11 @@
 from datetime import time
 from decimal import Decimal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class SService(BaseModel):
-    title: str
-    description: str
+    title: str = Field(..., max_length=255)
+    description: str | None
     price: Decimal
     lead_time: time
