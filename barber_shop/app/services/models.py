48f27e995e1ecca4e7bbe447f6baed9d67acf479
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-16 17:29:38 +0300

    services model

---
+++ barber_shop/app/services/models.py
@@ -0,0 +1,18 @@
+from datetime import time
+from decimal import Decimal
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Services(Base):
+    __tablename__ = 'services'
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    price: Mapped[Decimal] = mapped_column(nullable=False)
+    lead_time: Mapped[time] = mapped_column(nullable=False)
+    image: Mapped[bytes] = mapped_column(nullable=True)
