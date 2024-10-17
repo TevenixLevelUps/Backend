f123525bb43ba64c3dc45b6f1acc5b39ff998dbe
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-17 10:19:57 +0300

    service schemas

--- barber_shop/app/services/schemas.py
+++ barber_shop/app/services/schemas.py
@@ -1,11 +1,25 @@
 from datetime import time
 from decimal import Decimal
+from typing import TypeAlias, Annotated
+from uuid import UUID
 
-from pydantic import BaseModel, Field
+from annotated_types import MaxLen
+from pydantic import BaseModel
+
+ServiceTitle: TypeAlias = Annotated[str, MaxLen(255)]
 
 
-class SService(BaseModel):
-    title: str = Field(..., max_length=255)
+class SServiceCreate(BaseModel):
+    title: ServiceTitle
     description: str | None
     price: Decimal
     lead_time: time
+
+
+class SServiceGet(BaseModel):
+    id: UUID
+    title: ServiceTitle
+    description: str | None
+    price: Decimal
+    lead_time: time
+    image_id: UUID | None
