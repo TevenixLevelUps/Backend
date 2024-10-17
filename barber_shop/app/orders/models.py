83a7b83b8145b0e29ab5aa0d3b08f437574557ea
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-17 22:16:17 +0300

    order model

---
+++ barber_shop/app/orders/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.database import Base
+
+
+class Orders(Base):
+    __tablename__ = "orders"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    customer_name: Mapped[str] = mapped_column(nullable=False)
+    service_id: Mapped[UUID] = mapped_column(ForeignKey("services.id"), nullable=False)
+    specialist_id: Mapped[UUID] = mapped_column(ForeignKey("specialists.id"), nullable=False)
+    order_time: Mapped[datetime] = mapped_column(nullable=False)
