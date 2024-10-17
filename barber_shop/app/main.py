f87abf7e8b8a3640bf02a4b4d4a38bfe5665cf13
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-17 10:24:43 +0300

    include service router

--- barber_shop/app/main.py
+++ barber_shop/app/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 
 from app.config import settings
 from app.services.router import router as services_router
+from app.services.service_images.router import router as service_images_router
 
 app = FastAPI()
 
 app.include_router(services_router)
+app.include_router(service_images_router)
 
 if __name__ == "__main__":
     uvicorn.run(
