3325ed80028e2565ada2094e93a9d156fc7055d1
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-17 18:25:07 +0300

    get specialist endpoint

---
+++ barber_shop/app/specialists/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import session_getter
+from app.specialists.dao import SpecialistsDAO
+
+router = APIRouter(
+    prefix="/specialists",
+    tags=["Specialists"],
+)
+
+
+@router.get("/{specialist_name}/")
+async def get_specialist(
+        specialist_name: str,
+        session: AsyncSession = Depends(session_getter),
+):
+    specialist = await SpecialistsDAO.find_specialist_by_name(session, specialist_name)
+    return specialist
