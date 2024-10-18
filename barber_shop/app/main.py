6dfbf93914457d07cf17435e6abbd54eb5556e42
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-18 21:46:27 +0300

    cors

--- barber_shop/app/main.py
+++ barber_shop/app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.services.router import router as services_router
@@ -23,3 +24,22 @@ if __name__ == "__main__":
         port=settings.run.port,
         reload=True,
     )
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allow_headers=[
+        "Content-Type",
+        "Authorization",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+    ],
+)
