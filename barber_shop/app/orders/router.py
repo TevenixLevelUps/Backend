3aa7d7cf6896b3c36461a85f26ec1d3b665f8b02
Yauheni Mukhin <evgen2email@gmail.com>
2024-10-18 21:46:02 +0300

    get and delete order

--- barber_shop/app/orders/router.py
+++ barber_shop/app/orders/router.py
@@ -6,6 +6,8 @@ from app.database import session_getter
 from app.exceptions import WrongTimeException
 from app.orders.dao import OrdersDAO
 from app.orders.schemas import SOrderCreate
+from app.services.dao import ServicesDAO
+from app.specialists.dao import SpecialistsDAO
 
 router = APIRouter(
     prefix="/orders",
@@ -24,3 +26,30 @@ async def post_order(
         raise WrongTimeException
     await session.commit()
     return {"message": "order added successfully"}
+
+
+@router.get("/")
+async def get_orders(session: AsyncSession = Depends(session_getter)) -> list[SOrderCreate]:
+    orders = await OrdersDAO.find_all(session)
+    result_orders = []
+    for order in orders:
+        specialist = await SpecialistsDAO.find_one_or_none(session, id=order.specialist_id)
+        service = await ServicesDAO.find_one_or_none(session, id=order.service_id)
+        result_orders.append(
+            SOrderCreate(
+                customer_name=order.customer_name,
+                service_title=service.title,
+                specialist_name=specialist.name,
+                order_time=order.order_time
+            )
+        )
+    return result_orders
+
+
+@router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_order(
+        order_to_delete: SOrderCreate,
+        session: AsyncSession = Depends(session_getter),
+) -> None:
+    await OrdersDAO.delete_order(session, order_to_delete)
+    await session.commit()
