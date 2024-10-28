2033585b012ce9519150a6f444c4b20dd375a226
Dmitriy Pilevich <pilevichdima60@gmail.com>
2024-10-29 00:51:12 +0300

    Add alembic and cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
 from Orders.views import router as orders_router
@@ -20,9 +20,21 @@ async def lifespan(app: FastAPI):
 
 
 
-
-
 app = FastAPI(lifespan=lifespan)
+
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.include_router(specialist_router)
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+
+__all__ = ["Base","Orders","Service","Specialist"]
+
+from .base import Base
+from .orders import Orders
+from .service import Service
+from .specialist import Specialist
