1f883205bc868721a8d473e9806729da97e9c577
Yauheni Mukhin <evgen3email@gmail.com>
2024-12-03 18:54:07 +0300

    services router docs fix

--- services_service/app/services/service_images/router.py
+++ services_service/app/services/service_images/router.py
@@ -20,6 +20,7 @@ router = APIRouter(
         status_code=status.HTTP_201_CREATED,
         responses={
             status.HTTP_404_NOT_FOUND: {'model': ErrorSchema},
+            status.HTTP_409_CONFLICT: {'model': ErrorSchema},
             status.HTTP_415_UNSUPPORTED_MEDIA_TYPE: {'model': ErrorSchema},
         },
 )
