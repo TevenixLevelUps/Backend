560c088f8d4fb2f9fc9f71c00ba2aa7cbb590072
Yauheni Mukhin <evgen3email@gmail.com>
2024-12-03 18:52:55 +0300

    improved avatars router documentation

--- specialists_service/app/specialists/avatars/router.py
+++ specialists_service/app/specialists/avatars/router.py
@@ -6,6 +6,7 @@ from app.config import settings
 from app.database import session_getter
 from app.specialists.avatars.dao import SpecialistAvatarsDAO
 from app.specialists.dao import SpecialistsDAO
+from app.specialists.schemas import ErrorSchema
 
 router = APIRouter(
     prefix="/specialist_avatars",
@@ -13,18 +14,30 @@ router = APIRouter(
 )
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/", 
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_404_NOT_FOUND: {'model': ErrorSchema},
+        status.HTTP_409_CONFLICT: {'model': ErrorSchema},
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE: {'model': ErrorSchema},
+    },    
+)
 async def post_specialist_avatar(
         specialist_name: str,
         avatar: UploadFile = File(...),
         session: AsyncSession = Depends(session_getter),
-) -> dict[str, str]:
+) -> None:
     await SpecialistAvatarsDAO.create_specialist_avatar(session, specialist_name, avatar)
     await session.commit()
-    return {"message": "avatar added successfully"}
 
 
-@router.get("/")
+@router.get(
+    "/",
+    responses={
+        status.HTTP_404_NOT_FOUND: {'model': ErrorSchema},
+    },         
+)
 @cache(expire=settings.redis.cache_expire_seconds)
 async def get_avatar(
         specialist_name: str,
